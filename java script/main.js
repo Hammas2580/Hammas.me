5f61cb065cc9dea3f1bb9b3e04fc769b9e659e4d
Hammas2580 <hasirali653@gmail.com>
2024-10-10 12:09:16 +0500

    check images

--- java script/main.js
+++ java script/main.js
@@ -1,7 +1,7 @@
 // For navbar scroll behavior
-let nav = document.querySelector (".navbar");
+let nav = document.querySelector(".navbar");
 window.onscroll = function () {
-  if (document.documentElement.scrollTop >20) {
+  if (document.documentElement.scrollTop > 20) {
     nav.classList.add("header-scrolled ");
   } else {
     nav.classList.remove("header-scrolled");
@@ -16,14 +16,13 @@ navLinks.forEach(function (a) {
   });
 });
 
-
-document.querySelectorAll('.portfolio-card').forEach((card) => {
-  card.addEventListener('click', () => {
-    const overlay = card.querySelector('.overlay');
-    if (overlay.style.opacity === '1') {
-      overlay.style.opacity = '0'; // Hide buttons
+document.querySelectorAll(".portfolio-card").forEach((card) => {
+  card.addEventListener("click", () => {
+    const overlay = card.querySelector(".overlay");
+    if (overlay.style.opacity === "1") {
+      overlay.style.opacity = "0"; // Hide buttons
     } else {
-      overlay.style.opacity = '1'; // Show buttons
+      overlay.style.opacity = "1"; // Show buttons
     }
   });
 });
